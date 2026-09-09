241c42a5110f51266635f20f32663d049caf4b49
dipakarakharao3-cpu <dipakarakharao3@gmail.com>
2026-09-09 20:56:09 +0530

    Updated project files

--- Practise for Numpy.py
+++ Practise for Numpy.py
@@ -1,6 +1,16 @@
-
+#
+# import numpy as np
+#
+# arr = np.array([1, 2, 3, 4, 5])
+#
+# print(arr)
+#
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
 
 print(arr)
+
+print(type(arr))git add .
+git commit -m "Updated project files"
+git push
